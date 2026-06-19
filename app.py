3080bb1309a18d6b2c8f9c6f4e72f3e6dfc009b4
Andrea <andrea673.ac@gmail.com>
2026-06-18 22:48:44 -0600

    Agregar gunicorn

--- app.py
+++ app.py
@@ -227,5 +227,12 @@ def bebidas_all():
         "SELECT * FROM bebidas"
     )
     return jsonify(cursor.fetchall())
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    port = int(os.environ.get("PORT", 3000))
+
+    app.run(
+        host='0.0.0.0',
+        port=port
+    )
